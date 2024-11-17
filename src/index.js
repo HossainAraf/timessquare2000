038d164236e92a3cb3bdb83713991f31c4a59244
Hossain <arafat.kd99@gmail.com>
2024-11-17 13:18:15 +0600

    Import components & Add some routes

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './components/Home';
+import Sufism from './components/Sufism';
+import SufiQuotes from './components/SufiQuotes';
+import SufiPoetry from './components/SufiPoetry';
+import SufiMusic from './components/SufiMusic';
+import MuslimHistory from './components/MuslimHistory';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={< />} />
-        <Route path="/" element={< />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/sufism" element={<Sufism />} />
+        <Route path="/sufi-quotes" element={<SufiQuotes />} />
+        <Route path="/sufi-poetry" element={<SufiPoetry />} />
+        <Route path="/sufi-music" element={<SufiMusic />} />
+        <Route path="/muslim-history" element={<MuslimHistory />} />
       </Routes>
     </Router>
   );
