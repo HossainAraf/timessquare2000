6cdcdf6127f7c0b579865ee36fe280c808c8f3db
Hossain <arafat.kd99@gmail.com>
2024-11-18 21:30:46 +0600

    Fix Index.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Nav from './components/Nav';
+import Footer from './components/Footer';
+import Home from './components/Home';
+import Sufism from './components/Sufism';
+import SufiQuotes from './components/SufiQuotes';
+import SufiPoetry from './components/SufiPoetry';
+import SufiMusic from './components/SufiMusic';
+import MuslimHistory from './components/MuslimHistory';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header text-center">
-        <h1>Hello World ! </h1>
-      </header>
-    </div>
+    <Router>
+      <Nav />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sufism" element={<Sufism />} />
+        <Route path="/sufi-quotes" element={<SufiQuotes />} />
+        <Route path="/sufi-poetry" element={<SufiPoetry />} />
+        <Route path="/sufi-music" element={<SufiMusic />} />
+        <Route path="/muslim-history" element={<MuslimHistory />} />
+      </Routes>
+      <Footer />
+    </Router>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,29 +1,13 @@
-import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Nav from './components/Nav';
-import Footer from './components/Footer';
-import Home from './components/Home';
-import Sufism from './components/Sufism';
-import SufiQuotes from './components/SufiQuotes';
-import SufiPoetry from './components/SufiPoetry';
-import SufiMusic from './components/SufiMusic';
-import MuslimHistory from './components/MuslimHistory';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-function App() {
-  return (
-    <Router>
-      <Nav />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/sufism" element={<Sufism />} />
-        <Route path="/sufi-quotes" element={<SufiQuotes />} />
-        <Route path="/sufi-poetry" element={<SufiPoetry />} />
-        <Route path="/sufi-music" element={<SufiMusic />} />
-        <Route path="/muslim-history" element={<MuslimHistory />} />
-      </Routes>
-      <Footer />
-    </Router>
-  );
-}
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+);
 
-export default App;
+reportWebVitals();
