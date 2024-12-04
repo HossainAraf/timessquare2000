caa85f1151a0c523592701c5fa909d2fa13d597f
Hossain <arafat.kd99@gmail.com>
2024-12-05 00:31:32 +0600

    Update SufismPage content

--- src/components/Sufism/Sufism.jsx
+++ src/components/Sufism/Sufism.jsx
@@ -8,9 +8,9 @@ function SufismPage() {
       <Link to="/sufi-concept" className="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
         <img className="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg" src={arafat} alt="" />
         <div className="flex flex-col justify-between p-4 leading-normal">
-          <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
-          <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
-          <p>Read More</p>
+          <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Concept of Sufism</h5>
+          <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Sufism is the spiritual dimension of Islam, focusing on purifying the soul, cultivating divine love, and fostering a direct connection with Allah through introspection, devotion, and guidance from a spiritual mentor, the Murshid.</p>
+          <p>Read the articles</p>
         </div>
       </Link>
 
